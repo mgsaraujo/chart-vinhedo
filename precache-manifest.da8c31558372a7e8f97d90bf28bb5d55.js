ce0577d8f735045e5b3ab220d4a5b1a01fab1d82
cvt <cvt-2020@outlook.com>
2020-05-31 20:00:16 -0300

    UI

---
+++ precache-manifest.da8c31558372a7e8f97d90bf28bb5d55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f94dc44d6d4a18943629c309979af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaae65f2cfaf23ea4749",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eeaec4a4a223425a22b5",
+    "url": "/static/js/2.0314e5b6.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.0314e5b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaae65f2cfaf23ea4749",
+    "url": "/static/js/main.d1cd3a68.chunk.js"
+  },
+  {
+    "revision": "51c2afa818d0408b3ef1",
+    "url": "/static/js/runtime-main.3d8b4eef.js"
+  }
+]);
